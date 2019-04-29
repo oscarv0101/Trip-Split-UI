5a293e243932ff3c580200fa4efd56f45a55422d
milkslapper <oscar.valenzuela69@gmail.com>
2019-04-28 22:29:56 -0700

    added updated navigation to about us page

--- js/about-us.js
+++ js/about-us.js
@@ -42,3 +42,28 @@ TeamArray.map( p => {
         TweenMax.to(p.currentTarget, 1, {scale: 1, ease:Bounce.easeOut})
       })
     })
+    class MenuToggle {
+      constructor(element) {
+        this.element = element;
+        this.toggle = this.element.querySelector('.menuToggle'); 
+        this.closeMenu = this.element.querySelector('.close');
+        this.navMenu = this.element.querySelector('.navMenu');
+        //Click event on menutoggle fires popUp method
+        this.toggle.addEventListener('click', () => this.popUp());
+        //Click event on close element fires close method
+        this.closeMenu.addEventListener('click', () => this.close());
+      }
+      popUp() {
+        this.navMenu.classList.toggle('menuReveal')
+        this.toggle.style.display = 'none';
+      }
+      close() {
+        this.navMenu.classList.toggle('menuReveal');
+        this.toggle.style.display = 'flex';
+      }
+    }
+    
+    // Grabbing #nav element and passing intp constructor of MenuToggle Class
+    const menu = document.querySelector('#nav');
+    new MenuToggle(menu)
+    
